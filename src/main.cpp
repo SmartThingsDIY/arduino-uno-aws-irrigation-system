cc07a3b56287e252a1b9507ec7bbffe1aae56db8
iLyas <isbkch@gmail.com>
2020-10-01 22:32:33 -0400

    add debug information and send data to wifi

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,25 @@
+/**
+  @TODO ....
+  @author MecaHumArduino
+  @version 3.0
+*/
+
 #include <Arduino.h>
+#include <ArduinoJson.h> // https://github.com/bblanchon/ArduinoJson (use v6.xx)
+#include <SoftwareSerial.h>
+
+#define DEBUG true
+
+// ESP TX => Uno Pin 2
+// ESP RX => Uno Pin 3
+SoftwareSerial wifi(2, 3);
+
+// **************
+String sendDataToWiFiBoard(String command, const int timeout, boolean debug);
+String prepareDataForWiFi(float sensor1Value, float sensor2Value, float sensor3Value, float sensor4Value);
+void setup();
+void loop();
+// **************
 
 int IN1 = 2;
 int IN2 = 3;
@@ -14,6 +35,59 @@ float sensor1Value = 0;
 float sensor2Value = 0;
 float sensor3Value = 0;
 float sensor4Value = 0;
+
+/**
+ * Build and return a JSON document from the sensor data
+ * @param sensor1Value
+ * @param sensor2Value
+ * @param sensor3Value
+ * @param sensor4Value
+ * @return
+ */
+String prepareDataForWiFi(float sensor1Value, float sensor2Value, float sensor3Value, float sensor4Value)
+{
+  StaticJsonDocument<200> doc;
+
+  doc["sensor1Value"] = String(sensor1Value);
+  doc["sensor2Value"] = String(sensor2Value);
+  doc["sensor3Value"] = String(sensor3Value);
+  doc["sensor4Value"] = String(sensor4Value);
+
+  char jsonBuffer[512];
+  serializeJson(doc, jsonBuffer);
+
+  return jsonBuffer;
+}
+
+/**
+ * Send data through Serial to ESP8266 module
+ * @param command
+ * @param timeout
+ * @param debug
+ * @return
+ */
+String sendDataToWiFiBoard(String command, const int timeout, boolean debug)
+{
+  String response = "";
+
+  wifi.print(command); // send the read character to the esp8266
+
+  long int time = millis();
+
+  while((time+timeout) > millis()) {
+    while(wifi.available()) {
+      // The esp has data so display its output to the serial window
+      char c = wifi.read(); // read the next character.
+      response+=c;
+    }
+  }
+
+  if (debug) {
+    Serial.print(response);
+  }
+
+  return response;
+}
 
 void setup() {
   Serial.begin(9600);
@@ -37,6 +111,24 @@ void setup() {
 }
 
 void loop() {
+
+  if (DEBUG == true) {
+    Serial.print("buffer: ");
+    if (wifi.available()) {
+      String espBuf;
+      long int time = millis();
+
+      while((time+1000) > millis()) {
+        while (wifi.available()) {
+          // The esp has data so display its output to the serial window
+          char c = wifi.read(); // read the next character.
+          espBuf += c;
+        }
+      }
+      Serial.print(espBuf);
+    }
+    Serial.println(" endbuffer");
+  }
 
   Serial.print("Plant 1 - Moisture Level:");
   sensor1Value = analogRead(Pin1);
@@ -78,6 +170,11 @@ void loop() {
     digitalWrite(IN4, HIGH);
   }
 
-  Serial.println();
+  String preparedData = prepareDataForWiFi(sensor1Value, sensor2Value, sensor3Value, sensor4Value);
+  if (DEBUG == true) {
+    Serial.println(preparedData);
+  }
+  sendDataToWiFiBoard(preparedData, 1000, DEBUG);
+
   delay(1000);
 }
